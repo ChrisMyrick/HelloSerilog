dafe064bf20b22cf88da65db656cc89b797e1388
Chris Myrick <ChrisM@svmic.com>
2018-11-15 14:32:50 -0600

    Stub Azure Integration project

--- BasicSerilog/Program.cs
+++ BasicSerilog/Program.cs
@@ -3,13 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace BasicSerilog
 {
     class Program
     {
         static void Main(string[] args)
+        { 
+            LogToConsole();
+            LogToFile();
+            LogParameters();
+        }
+
+        /// <summary>
+        /// Example of logging to the console window
+        /// </summary>
+        private static void LogToConsole()
         {
+            // A LoggerConfiguration is used to create and assign the default logger.
+            var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            // The simplest way to set up Serilog is using the static Log class.
+            // Apply the configuration to the static logger
+            Log.Logger = log;
+
+            Log.Information("The static log object has been configured.");
+            Log.Warning("This is a warning message.");
+        }
+
+        /// <summary>
+        /// Example of logging to a rolling log file
+        /// </summary>
+        private static void LogToFile()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+        }
+
+        /// <summary>
+        /// Example of converting params to JSON and output to log file
+        /// </summary>
+        private static void LogParameters()
+        {
+            var position = new { Latitude = 25, Longitude = 134 };
+            var elapsedMs = 34;
+
+            // The @ operator in front of Position tells Serilog to serialize the object
+            // passed in, rather than convert it using ToString().
+            Log.Information("Processed {@Position} in {Elapsed} ms.", position, elapsedMs);
+
+            Console.ReadKey();
+            Log.CloseAndFlush();
         }
     }
 }
--- SerilogToAzure/Properties/AssemblyInfo.cs
+++ SerilogToAzure/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloSerilog")]
+[assembly: AssemblyTitle("SerilogToAzure")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloSerilog")]
+[assembly: AssemblyProduct("SerilogToAzure")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7c656934-cec6-4b47-a6f9-fdc7726e90ba")]
+[assembly: Guid("5cd52ced-7794-416e-955a-f0d4130ad4f1")]
 
 // Version information for an assembly consists of the following four values:
 //
